c074ab950d9ede43016485dbe8a66c3b03a564b5
Tess <tessaghuang@gmail.com>
2023-06-27 12:14:53 -0400

    Header START

--- SafePath/ContentView.swift
+++ SafePath/ContentView.swift
@@ -10,19 +10,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+            // Customize the navigation bar appearance
+            let appearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(Color("darkColor"))
+            appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+            
+            
+            UINavigationBar.appearance().standardAppearance = appearance
+            UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        }
+    
     var body: some View {
         
-        ZStack{
-            Color("mainColor")
-                .ignoresSafeArea()
+        NavigationView {
+            ZStack{
+                Color("mainColor")
+                    .ignoresSafeArea()
+                    VStack {
+                        // PUT MAIN STUFF HERE !!
+                    }//end of VSTACK (with initial img and text)
+                .padding()
+            }//end of ZSTACK (app background color)
+                    .navigationBarItems( leading:
+                                        
+                                            Text("put items here")
+                        .font(.title)
+                        )
+                    
+                }
+            }
+        }
+
+        struct DetailView: View {
+            var body: some View {
                 VStack {
-                    Image(systemName: "globe")
-                        .imageScale(.large)
-                        .foregroundColor(.accentColor)
-                    Text("I WANNA SLEEP 😴!")
-                }//end of VSTACK (with initial img and text)
-            .padding()
-        }//end of ZSTACK (app background color)
+                    Text("Detail Content")
+                        .font(.largeTitle)
+                    
+                    NavigationLink(destination: SubDetailView()) {
+                        Text("Go to Subdetail")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                }
+                .navigationBarTitle("Detail")
+            }
+        }
+
+        struct SubDetailView: View {
+            var body: some View {
+                Text("Subdetail Content")
+                    .font(.largeTitle)
+                    .navigationBarTitle("Subdetail")
+
+        
+        
     } //end of BODY
 }//end of STRUCT
 
