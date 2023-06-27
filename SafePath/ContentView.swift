a8cd5211621abbe2bef9b07b4d1a8dee72d4b0ff
Emma Tse <eclairnc@gmail.com>
2023-06-27 10:03:08 -0400

    I am trying a change that will be on the preview

--- SafePath/ContentView.swift
+++ SafePath/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("I WANNA SLEEP 😴!")
         }
         .padding()
     }
