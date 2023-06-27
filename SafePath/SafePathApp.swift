fcb58a0bf69c2a051b010a70ee3622d6c059b39c
Emma Tse <eclairnc@gmail.com>
2023-06-27 09:29:59 -0400

    Initial Commit

---
+++ SafePath/SafePathApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafePathApp.swift
+//  SafePath
+//
+//  Created by scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SafePathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
